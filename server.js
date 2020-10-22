95d75fd61a8f584aa5c6bfd303a5cd7fa28d1e8a
VitorFigm <vitorfigmarques@hotmail.com>
2020-10-22 07:52:32 -0300

    cors update

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
-app.use((req,res,next)=>{
-    res.header('Acess-Control-Allow-Origin','*')
-    app.use(cors)
-    next()
-})
+app.use(cors())
 
 ///defining static folder
 const static_path = path.join(__dirname,"build")
